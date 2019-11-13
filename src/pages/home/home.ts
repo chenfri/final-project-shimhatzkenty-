623f7697b7b87141cdb187d30f0e385bbc6d586d
Yarden <yarden.paniry@gmail.com>
2019-11-13 16:18:43 +0200

    homePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,11 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
+
+  // logout() {
+  //   this.fAuth.auth.signOut();
+  //   this.navCtrl.push(LoginPage);
+  // }
 
 }
