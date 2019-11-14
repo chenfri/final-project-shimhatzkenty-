03895e2fc47e79238f3698964e187b6bb609f94a
chen <chenfriedman93@gmail.com>
2019-11-14 16:29:43 +0200

    ש

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -73,6 +73,39 @@ export class Form
     alert.present();
   }
 
+  
+  showAlert()
+  {
+    let alert = this.alertCtrl.create({
+      title: '!הפרטים נשמרו בהצלחה',
+      subTitle: 'שים לב, יש למלא טופס פרטים אישיים' ,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+
+ async registry()
+  {
+    console.log(this.user.email);
+    console.log(this.user.password);
+    try{
+      const res = await firebase.auth().createUserWithEmailAndPassword
+      (this.user.email, this.user.password);
+      if(res)
+      {
+        //console.log(res.user.uid);
+        this.showAlert();
+        this.navCtrl.push(HomePage);
+      }
+    }
+    catch(e)
+    {
+      console.error(e);
+    }
+  }
+
+
 
   //check all user inputs are legal
   check_field_value()
