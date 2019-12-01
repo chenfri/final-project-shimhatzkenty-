136096e61d1a715b95d5ce40a41269c92147143a
chen <chenfriedman93@gmail.com>
2019-12-01 17:17:37 +0200

    admin page

--- src/pages/Admin/adminPage.ts
+++ src/pages/Admin/adminPage.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../../module/User'
 import { NavController,NavParams} from 'ionic-angular';
 import firebase, { firestore } from 'firebase';
+import { map } from 'rxjs/operator/map';
 
 @Component({
   selector: 'adminPage',
@@ -10,56 +11,41 @@ import firebase, { firestore } from 'firebase';
 
 export class adminPage
  {
-  currentEvent:any;
-
-
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) 
-  { }
+  {
+   this.get_data_from_firebase();
+    //console.log(this.temp)
+   }
 
   user = {} as User;
   public hobbies: any[] 
   public time: any[]
   public numOfMeeting: any[]
 
-  
 
   get_data_from_firebase()
   {
+    let tmp =[]
+    let i = 0
     const db = firebase.firestore();
-
-    if(this.user.elderly)
+    db.collection("ElderlyUsers").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          //console.log(doc.id, " => ", doc.data());
+          tmp[i] = doc.data()
+          console.log(tmp[i])
+          i++
+       });
+   //    console.log(this.temp)
+      // return tmp
+       
+  });
+   /* const result = db.collection('ElderlyUsers').get().then(res =>
     {
-      db.collection('ElderlyUsers').doc(firebase.auth().currentUser.uid).get()
-      .then(result =>{
-        if (!result.exists) return
-          this.user.fullName = result.data().fullName;
-          this.user.address = result.data().address;
-          this.user.phone = result.data().phone
-          this.user.email = result.data().email
-          this.user.onBehalf = result.data().behalf
-          this.user.nameAssistant = result.data().nameAssistant
-          this.user.relationship = result.data().relationship
-          this.hobbies = result.data().hobbies,
-          this.time = result.data().meeting_time
-       })
-    }
-    else
-    {
-      db.collection('volunteerUsers').doc(firebase.auth().currentUser.uid).get()
-      .then(result =>{
-        if (!result.exists) return
-          this.user.fullName = result.data().fullName;
-          this.user.address = result.data().address;
-          this.user.phone = result.data().phone
-          this.user.email = result.data().email
-          this.hobbies = result.data().hobbies
-          this.user.range = result.data().range,
-          this.time = result.data().meeting_time
-          this.numOfMeeting = result.data().num_of_meetings
-       })
-    }
-  
-
+         res.forEach(i => {console.log(i.data())})
+    
+    })*/
   }
   
 }
