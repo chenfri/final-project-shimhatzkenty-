76d71f56f206a6260c9076383adb5ca074ecb1df
Yarden <yarden.paniry@gmail.com>
2020-05-27 12:52:53 +0300

    fix2

--- src/pages/match/match.ts
+++ src/pages/match/match.ts
@@ -117,15 +117,17 @@ export class MatchPage {
   }
 
 
-  CancelMatch(idE)
+  CancelMatch(idE,idV)
   {
     const db = firebase.firestore();    
     db.collection("ElderlyUsers").doc(idE).update({
        matching: ["",0],
        status: 0
     }) 
-    db.collection("volunteerUsers").doc(this.IDlogged).update({
-      status: 3}) 
+    db.collection("volunteerUsers").doc(idV).update({
+      matching: null,
+      status: 3
+    }) 
 
    this.cancelText = true
   }
